2fe26d0013aeb7b690a11b21212bd49b23a6aebc
Diana Lucia Avila <davila@fivetech.co>
2019-03-20 17:43:56 -0500

    Configuración de modelos, controladores y rutas - Inicial

--- app/Actividades.php
+++ app/Actividades.php
@@ -6,5 +6,22 @@ use Illuminate\Database\Eloquent\Model;
 
 class Actividades extends Model
 {
-    //
+  protected $table = "Actividades";
+
+  protected $fillable =
+  [
+    'ac_titulo',
+    'ac_descripcion',
+    'ac_fecha_programada',
+    'categoria_id',
+    'estados_id'
+  ];
+
+  public function categorias(){
+    return $this->belogsTo('App\Categorias');
+  }
+
+  public function estadosActividad(){
+    return $this->belogsTo('App\EstadosActividad');
+  }
 }
--- app/EstadosActividad.php
+++ app/EstadosActividad.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class EstadosActividad extends Model
 {
-    //
+    protected $table = "EstadosActividad";
+
+    protected $fillable = ['es_descripcion'];
+
+    public function actividades(){
+      return $this->hasMany('App\Actividades');
+    }
 }
